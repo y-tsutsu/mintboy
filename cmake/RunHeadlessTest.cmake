012473d282dd42a353d06bae8c1ee05f5309aa8f
tsutsu <tsutsu0928@gmail.com>
2026-05-28 00:21:54 +0900

    Blarggの追加ROMテストを登録

--- cmake/RunHeadlessTest.cmake
+++ cmake/RunHeadlessTest.cmake
@@ -10,6 +10,10 @@ if(NOT DEFINED MINTBOY_FRAMES)
     set(MINTBOY_FRAMES 600)
 endif()
 
+if(NOT DEFINED MINTBOY_EXPECTED_OUTPUT)
+    set(MINTBOY_EXPECTED_OUTPUT "Passed")
+endif()
+
 execute_process(
     COMMAND "${MINTBOY_HEADLESS}" "${MINTBOY_ROM}" "${MINTBOY_FRAMES}"
     RESULT_VARIABLE result
@@ -23,10 +27,10 @@ if(NOT result EQUAL 0)
     message(FATAL_ERROR "headless test failed with exit code ${result}")
 endif()
 
-string(FIND "${output}" "Passed" passed_index)
+string(FIND "${output}" "${MINTBOY_EXPECTED_OUTPUT}" passed_index)
 if(passed_index EQUAL -1)
     message("${output}")
-    message(FATAL_ERROR "headless test did not report Passed")
+    message(FATAL_ERROR "headless test did not report ${MINTBOY_EXPECTED_OUTPUT}")
 endif()
 
 message("${output}")
